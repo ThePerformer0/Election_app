b90b031fa6683987f3a326846205df8988a59e08
ThePerformer <fjimmywilson@gmail.com>
2025-02-09 15:15:05 +0100

    modification of the structure

--- election_project/vote/apps.py
+++ election_project/vote/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class VoteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'vote'
